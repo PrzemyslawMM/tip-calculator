dc789b31469244a0ac7504a96da27c01e7164ea0
Przemeks200iq <vertezelopl@gmail.com>
2022-07-11 14:29:35 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  devtool: 'eval-source-map',
+  output: {
+    publicPath: 'public',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  target: 'web',
+  devServer: {
+    open: true,
+    static: './public',
+    port: 8080,
+    hot: false,
+  },
+};
